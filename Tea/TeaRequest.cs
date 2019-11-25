43a6dd6e9b01d45f16aa3cd9f8e786567ef8d408
liaoyustudent <wb-ly617236@alibaba-inc.com>
2019-11-25 18:00:02 +0800

    improved TeaRequest

--- Tea/TeaRequest.cs
+++ Tea/TeaRequest.cs
@@ -5,22 +5,76 @@ namespace Tea
 {
     public class TeaRequest
     {
+        private string _protocol;
+        private Dictionary<string, string> _query;
+        private Dictionary<string, string> _headers;
         public TeaRequest()
         {
             Query = new Dictionary<string, string>();
             Headers = new Dictionary<string, string>();
         }
-        public string Protocol;
+        public string Protocol
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_protocol))
+                {
+                    return "http";
+                }
+                else
+                {
+                    return _protocol;
+                }
+            }
+            set
+            {
+                _protocol = value;
+            }
+        }
 
-        public int Port;
+        public int Port { get; set; }
 
-        public string Method;
+        public string Method { get; set; }
 
-        public string Pathname;
+        public string Pathname { get; set; }
 
-        public Dictionary<string, string> Query;
+        public Dictionary<string, string> Query
+        {
+            get
+            {
+                if (_query == null)
+                {
+                    return new Dictionary<string, string>();
+                }
+                else
+                {
+                    return _query;
+                }
+            }
+            set
+            {
+                _query = value;
+            }
+        }
 
-        public Dictionary<string, string> Headers;
+        public Dictionary<string, string> Headers
+        {
+            get
+            {
+                if (_headers == null)
+                {
+                    return new Dictionary<string, string>();
+                }
+                else
+                {
+                    return _headers;
+                }
+            }
+            set
+            {
+                _headers = value;
+            }
+        }
 
         public Stream Body;
     }
