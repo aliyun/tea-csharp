34894f2aea88c51f73dbe49790a4b1b11010104c
liaoyustudent <wb-ly617236@alibaba-inc.com>
2019-12-19 17:01:37 +0800

    supported teaResponse.Body

--- Tea/TeaResponse.cs
+++ Tea/TeaResponse.cs
@@ -1,17 +1,27 @@
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 
 namespace Tea
 {
     public class TeaResponse
     {
+
+        private HttpWebResponse _response { get; set; }
+
         public int StatusCode { get; set; }
 
         public string StatusMessage { get; set; }
 
         public Dictionary<string, string> Headers { get; set; }
 
-        public HttpWebResponse _Response { get; set; }
+        public Stream Body
+        {
+            get
+            {
+                return _response.GetResponseStream();
+            }
+        }
 
         public TeaResponse(HttpWebResponse response)
         {
@@ -20,7 +30,7 @@ namespace Tea
                 StatusCode = (int) response.StatusCode;
                 StatusMessage = response.StatusDescription;
                 Headers = TeaCore.ConvertHeaders(response.Headers);
-                _Response = response;
+                _response = response;
             }
         }
     }
