355be8e6bc54b2461a4bbe07a34a4208a275b974
liaoyustudent <wb-ly617236@alibaba-inc.com>
2019-12-31 14:50:58 +0800

    supported teaRetryableException

---
+++ Tea/TeaRetryableException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tea
+{
+    public class TeaRetryableException : Exception
+    {
+        public TeaRequest Request { get; set; }
+
+        public TeaResponse Response { get; set; }
+
+        public TeaRetryableException(TeaRequest request, TeaResponse response)
+        {
+            Request = request;
+            Response = response;
+        }
+    }
+}
---
+++ TeaUnitTests/TeaRetryableExceptionTest.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+using Moq;
+
+using Tea;
+
+using Xunit;
+
+namespace TeaUnitTests
+{
+    public class TeaRetryableExceptionTest
+    {
+        [Fact]
+        public void TestTeaRetryableException()
+        {
+            Mock<HttpWebResponse> mock = new Mock<HttpWebResponse>();
+            mock.Setup(p => p.StatusCode).Returns(HttpStatusCode.OK);
+            mock.Setup(p => p.Headers).Returns(new WebHeaderCollection());
+            mock.Setup(p => p.GetResponseStream()).Returns(new MemoryStream(Encoding.UTF8.GetBytes("test")));
+            TeaResponse response = new TeaResponse(mock.Object);
+            TeaRetryableException teaRetryableException = new TeaRetryableException(new TeaRequest(), response);
+            Assert.NotNull(teaRetryableException);
+            Assert.NotNull(teaRetryableException.Request);
+            Assert.NotNull(teaRetryableException.Response);
+        }
+    }
+}
