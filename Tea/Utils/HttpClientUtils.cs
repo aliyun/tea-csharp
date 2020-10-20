8eb0bbb13a5884421a66074b8eebbc054f982179
liaoyustudent <wb-ly617236@alibaba-inc.com>
2020-10-20 14:26:29 +0800

    supported verify cert

--- Tea/Utils/HttpClientUtils.cs
+++ Tea/Utils/HttpClientUtils.cs
@@ -1,8 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
 
 namespace Tea.Utils
 {
@@ -40,7 +49,63 @@ namespace Tea.Utils
             HttpClient httpClient;
             string proxyUrl = options.Get("httpProxy") != null ? options.Get("httpProxy").ToSafeString() : options.Get("httpsProxy").ToSafeString();
             bool ignoreSSL = options.Get("ignoreSSL").ToSafeBool(false);
+            string certStr = options.Get("cert").ToSafeString();
+            string keyStr = options.Get("key").ToSafeString();
+            string caStr = options.Get("ca").ToSafeString();
+            X509Certificate2Collection CaCerts = new X509Certificate2Collection();
+            Org.BouncyCastle.X509.X509Certificate pemCert;
+            AsymmetricKeyParameter pemKey;
+            X509Certificate2 x509Cert = new X509Certificate2(new byte[0]);
+
+            if (!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
+            {
+                byte[] certOption = Encoding.UTF8.GetBytes(certStr);
+                byte[] keyOption = Encoding.UTF8.GetBytes(keyStr);
+
+                if(!string.IsNullOrWhiteSpace(caStr))
+                {
+                    byte[] caOption = Encoding.UTF8.GetBytes(caStr);
+                    X509Certificate2 rooCert = new X509Certificate2(caOption);
+                    CaCerts.Add(rooCert);
+                }
+                
+
+                pemCert = new X509CertificateParser().ReadCertificate(new MemoryStream(certOption));
+
+                object obj;
+                using (var reader = new StreamReader(new MemoryStream(keyOption)))
+                {
+                    obj = new PemReader(reader).ReadObject();
+                    var keypair = obj as AsymmetricCipherKeyPair;
+                    if (keypair != null)
+                    {
+                        var cipherKey = keypair;
+                        obj = cipherKey.Private;
+                    }
+                }
+
+                pemKey = (AsymmetricKeyParameter)obj;
+                var builder = new Pkcs12StoreBuilder();
+                builder.SetUseDerEncoding(true);
+                var store = builder.Build();
+                var certEntry = new X509CertificateEntry(pemCert);
+                store.SetCertificateEntry("", certEntry);
+                store.SetKeyEntry("", new AsymmetricKeyEntry(pemKey), new[] { certEntry });
+                byte[] data;
+                using (var ms = new MemoryStream())
+                {
+                    store.Save(ms, new char[0], new SecureRandom());
+                    data = ms.ToArray();
+                }
+
+                x509Cert = new X509Certificate2(data);
+            }
+
 #if NET45
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3
+                                       | SecurityProtocolType.Tls
+                                       | SecurityProtocolType.Tls11
+                                       | SecurityProtocolType.Tls12;
             var handler = new WebRequestHandler();
             if(!string.IsNullOrWhiteSpace(proxyUrl))
             {
@@ -50,6 +115,20 @@ namespace Tea.Utils
             {
                 handler.UseProxy = false;
             }
+
+            if(!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
+            {
+                handler.ClientCertificates.Add(x509Cert);
+                if(!ignoreSSL)
+                {
+                    handler.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+                    {
+                        return CertificateValidationCallBack(sender, CaCerts, certificate, chain,
+                            sslPolicyErrors);
+                    };
+                }
+            }
+
             if(ignoreSSL)
             {
                 handler.ServerCertificateValidationCallback = delegate { return true; };
@@ -65,6 +144,20 @@ namespace Tea.Utils
             {
                 httpClientHandler.UseProxy = false;
             }
+
+            if(!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
+            {
+                httpClientHandler.ClientCertificates.Add(x509Cert);
+                if(!ignoreSSL)
+                {
+                    httpClientHandler.ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
+                    {
+                        return CertificateValidationCallBack(sender, CaCerts, certificate, chain,
+                            sslPolicyErrors);
+                    };
+                }
+            }
+
             if (ignoreSSL)
             {
                 httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
@@ -73,6 +166,54 @@ namespace Tea.Utils
 #endif
             return httpClient;
 
+        }
+
+
+        public static bool CertificateValidationCallBack(
+            object sender,
+            X509Certificate2Collection caCerts,
+            System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+            X509Chain chain,
+            SslPolicyErrors sslPolicyErrors)
+        {
+            // If the certificate is a valid, signed certificate, return true.
+            if (sslPolicyErrors == SslPolicyErrors.None)
+            {
+                return true;
+            }
+
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if ((sslPolicyErrors & SslPolicyErrors.RemoteCertificateChainErrors) != 0)
+            {
+                chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+
+                // Added our trusted certificates to the chain
+                //
+                chain.ChainPolicy.ExtraStore.AddRange(caCerts);
+
+                chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
+                var isValid = chain.Build((X509Certificate2)certificate);
+
+                var isTrusted = false;
+
+                var rootCert = chain.ChainElements[chain.ChainElements.Count - 1].Certificate;
+
+                // Make sure that one of our trusted certs exists in the chain provided by the server.
+                //
+                foreach (var cert in caCerts)
+                {
+                    if (rootCert.RawData.SequenceEqual(cert.RawData))
+                    {
+                        isTrusted = true;
+                        break;
+                    }
+                }
+
+                return isValid && isTrusted;
+            }
+
+            // In all other cases, return false.
+            return false;
         }
     }
 }
