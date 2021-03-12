273e6f474e9a9db4c5e0316684bbbb37c2ef17c5
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2021-03-12 14:14:00 +0800

    deprecated: BouncyCastle

--- Tea/Utils/HttpClientUtils.cs
+++ Tea/Utils/HttpClientUtils.cs
@@ -9,12 +9,6 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.OpenSsl;
-using Org.BouncyCastle.Pkcs;
-using Org.BouncyCastle.Security;
-using Org.BouncyCastle.X509;
-
 namespace Tea.Utils
 {
     public static class HttpClientUtils
@@ -51,57 +45,6 @@ namespace Tea.Utils
             HttpClient httpClient;
             string proxyUrl = options.Get("httpProxy") != null ? options.Get("httpProxy").ToSafeString() : options.Get("httpsProxy").ToSafeString();
             bool ignoreSSL = options.Get("ignoreSSL").ToSafeBool(false);
-            string certStr = options.Get("cert").ToSafeString();
-            string keyStr = options.Get("key").ToSafeString();
-            string caStr = options.Get("ca").ToSafeString();
-            X509Certificate2Collection CaCerts = new X509Certificate2Collection();
-            Org.BouncyCastle.X509.X509Certificate pemCert;
-            AsymmetricKeyParameter pemKey;
-            X509Certificate2 x509Cert = new X509Certificate2(new byte[0]);
-
-            if (!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
-            {
-                byte[] certOption = Encoding.UTF8.GetBytes(certStr);
-                byte[] keyOption = Encoding.UTF8.GetBytes(keyStr);
-
-                if(!string.IsNullOrWhiteSpace(caStr))
-                {
-                    byte[] caOption = Encoding.UTF8.GetBytes(caStr);
-                    X509Certificate2 rooCert = new X509Certificate2(caOption);
-                    CaCerts.Add(rooCert);
-                }
-                
-
-                pemCert = new X509CertificateParser().ReadCertificate(new MemoryStream(certOption));
-
-                object obj;
-                using (var reader = new StreamReader(new MemoryStream(keyOption)))
-                {
-                    obj = new PemReader(reader).ReadObject();
-                    var keypair = obj as AsymmetricCipherKeyPair;
-                    if (keypair != null)
-                    {
-                        var cipherKey = keypair;
-                        obj = cipherKey.Private;
-                    }
-                }
-
-                pemKey = (AsymmetricKeyParameter)obj;
-                var builder = new Pkcs12StoreBuilder();
-                builder.SetUseDerEncoding(true);
-                var store = builder.Build();
-                var certEntry = new X509CertificateEntry(pemCert);
-                store.SetCertificateEntry("", certEntry);
-                store.SetKeyEntry("", new AsymmetricKeyEntry(pemKey), new[] { certEntry });
-                byte[] data;
-                using (var ms = new MemoryStream())
-                {
-                    store.Save(ms, new char[0], new SecureRandom());
-                    data = ms.ToArray();
-                }
-
-                x509Cert = new X509Certificate2(data);
-            }
 
 #if NET45
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3
@@ -116,19 +59,6 @@ namespace Tea.Utils
             else
             {
                 handler.UseProxy = false;
-            }
-
-            if(!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
-            {
-                handler.ClientCertificates.Add(x509Cert);
-                if(!ignoreSSL)
-                {
-                    handler.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
-                    {
-                        return CertificateValidationCallBack(sender, CaCerts, certificate, chain,
-                            sslPolicyErrors);
-                    };
-                }
             }
 
             if(ignoreSSL)
@@ -146,20 +76,7 @@ namespace Tea.Utils
             {
                 httpClientHandler.UseProxy = false;
             }
-
-            if(!string.IsNullOrWhiteSpace(certStr) && !string.IsNullOrWhiteSpace(keyStr))
-            {
-                httpClientHandler.ClientCertificates.Add(x509Cert);
-                if(!ignoreSSL)
-                {
-                    httpClientHandler.ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) =>
-                    {
-                        return CertificateValidationCallBack(sender, CaCerts, certificate, chain,
-                            sslPolicyErrors);
-                    };
-                }
-            }
-
+            
             if (ignoreSSL)
             {
                 httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
