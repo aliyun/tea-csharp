d81c2c4f43d00a73c5a5928adf9a5b2dc003f969
nanhe <papaya.dyn@alibaba-inc.com>
2022-10-16 15:59:34 +0800

    feat: return more detail in error info

--- TeaUnitTests/TeaExceptionTest.cs
+++ TeaUnitTests/TeaExceptionTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 
 using Tea;
+using Tea.Utils;
 
 using Xunit;
 
@@ -54,6 +55,49 @@ namespace TeaUnitTests
             });
             Assert.NotNull(teaException);
             Assert.Equal("200", teaException.Code);
+
+            teaException = new TeaException(new Dictionary<string, object>
+            { { "code", "code" },
+                { "message", "message" },
+                { "description", "description" },
+                {
+                    "data",
+                    new Dictionary<string, object>
+                    { { "test", "test" },
+                        {"statusCode", 200}
+                    }
+                },
+                {
+                    "accessDeniedDetail",
+                    new Dictionary<string, object>
+                    { { "NoPermissionType", "ImplicitDeny" }
+                    }
+                }
+            });
+            Assert.NotNull(teaException);
+            Assert.Equal("code", teaException.Code);
+            Assert.Equal("message", teaException.Message);
+            Assert.Equal("description", teaException.Description);
+            Assert.Equal(200, teaException.StatusCode);
+            Assert.Equal("ImplicitDeny", DictUtils.GetDicValue(teaException.AccessDeniedDetail, "NoPermissionType"));
+
+            teaException = new TeaException(new Dictionary<string, object>
+            { { "code", "code" },
+                {
+                    "accessDeniedDetail", null
+                }
+            });
+            Assert.NotNull(teaException);
+            Assert.Null(teaException.AccessDeniedDetail);
+
+            teaException = new TeaException(new Dictionary<string, object>
+            { { "code", "code" },
+                {
+                    "accessDeniedDetail", "error type"
+                }
+            });
+            Assert.NotNull(teaException);
+            Assert.Null(teaException.AccessDeniedDetail);
         }
     }
 }
