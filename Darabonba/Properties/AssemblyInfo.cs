d4d1ec9a09e1326e84b0919272ca5df5fd6a9199
zpp <865287328@qq.com>
2025-12-30 14:35:52 +0800

    Bump 1.0.1

--- Darabonba/Properties/AssemblyInfo.cs
+++ Darabonba/Properties/AssemblyInfo.cs
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: InternalsVisibleTo("DaraUnitTests")]
 
